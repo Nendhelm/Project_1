84391a699009162034b01959bc1b1497fcfd78ab
Dobr <nendhelm@gmail.com>
2025-09-02 19:25:34 +0300

    feat: add mul operation

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ def sum_calc(a: float, b: float) -> float:
     return a + b
 
 
+def mul_calc(a: float, b: float) -> float:
+    return a * b
+
+
 a = float(input())
 b = float(input())
 
-summ = sum_calc(a=a, b=b)
-print(summ)
+c = sum_calc(a=a, b=b)
+d = mul_calc(a=a, b=b)
+print(d)
